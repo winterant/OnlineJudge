fdbe0facb0f55a8ce56b9c20d1c4740aabcb305c
iamwinter <15866997163@163.com>
2020-01-26 00:27:23 +0800

    debug

--- install/ubuntu16.04/install.sh
+++ install/ubuntu16.04/install.sh
@@ -16,7 +16,7 @@ apt-get -y update
 apt -y install php7.2 php7.2-fpm php7.2-mysql
 
 # composer
-apt -y install composer
+apt -y install composer zip unzip
 cd ${web_home}/LDUOnlineJudge && composer install --ignore-platform-reqs
 
 # laravel artisan; 必须在composer之后
@@ -28,19 +28,20 @@ php artisan route:cache
 
 # nignx
 apt -y install nginx
-rm -rf /etc/nginx/sites-available/default
+rm -rf /etc/nginx/sites-enabled/default
 cp -f ${web_home}/LDUOnlineJudge/install/nginx/lduoj.conf  /etc/nginx/conf.d/lduoj.conf
 service nginx restart
 
 # mysql
 apt -y install mysql-server libmysqlclient-dev
+service mysql restart
 USER=`cat /etc/mysql/debian.cnf |grep user|head -1|awk '{print $3}'`
 PASSWORD=`cat /etc/mysql/debian.cnf |grep password|head -1|awk '{print $3}'`
-mysql -h localhost -u${USER} -p${PASSWORD} -e"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'rootroot';"
-mysql -h localhost -u${USER} -p${PASSWORD} -e"CREATE DATABASE lduoj;"
-mysql -h localhost -u${USER} -p${PASSWORD} -e"CREATE USER 'lduoj'@'localhost' IDENTIFIED BY '123456789';"
-mysql -h localhost -u${USER} -p${PASSWORD} -e"GRANT all privileges ON lduoj.* TO 'lduoj'@'localhost' identified by '123456789';flush privileges;"
-mysql -h localhost -u${USER} -p${PASSWORD} -Dlduoj < ${web_home}/LDUOnlineJudge/install/mysql/lduoj.sql
+mysql -u${USER} -p${PASSWORD} -e"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'rootroot';"
+mysql -u${USER} -p${PASSWORD} -e"CREATE DATABASE lduoj;"
+mysql -u${USER} -p${PASSWORD} -e"CREATE USER 'lduoj'@'localhost' IDENTIFIED BY '123456789';"
+mysql -u${USER} -p${PASSWORD} -e"GRANT all privileges ON lduoj.* TO 'lduoj'@'localhost' identified by '123456789';flush privileges;"
+mysql -u${USER} -p${PASSWORD} -Dlduoj < ${web_home}/LDUOnlineJudge/install/mysql/lduoj.sql
 
 
 # C/C++
