efe529384436f26d50df7c33feb6b80edb72d01a
iamwinter <15866997163@163.com>
2020-02-07 11:36:48 +0800

    LDU Online Judge

--- config/logging.php
+++ config/logging.php
@@ -51,6 +51,7 @@ return [
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
             'days' => 14,
+            "permission"=>0777,
         ],
 
         'slack' => [
