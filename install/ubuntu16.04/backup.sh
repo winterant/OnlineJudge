f8f7687158c8b69ac2ec7c71c77d12258ec0c9b9
赵京龙 <winterant@163.com>
2020-03-09 15:45:44 +0800

    LDU Online Judge

--- install/ubuntu16.04/backup.sh
+++ install/ubuntu16.04/backup.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-set -x
-
 web_home=/home    #项目存放位置
 backup='lduoj_'$(date "+%Y%m%d_%H%M%S")
 
-# 项目备份
+# 备份整个项目，包括storage
 if [ ! -d ${web_home}/lduoj_backup/${backup} ];then
   mkdir -p ${web_home}/lduoj_backup/${backup}
 fi;
-mv -f ${web_home}/LDUOnlineJudge ${web_home}/lduoj_backup/${backup}
+cp -r -f -p ${web_home}/LDUOnlineJudge ${web_home}/lduoj_backup/${backup}/
 
 # 数据库备份
 USER=`cat /etc/mysql/debian.cnf |grep user|head -1|awk '{print $3}'`
@@ -18,6 +16,7 @@ mysqldump -u${USER} -p${PASSWORD} -B lduoj > ${web_home}/lduoj_backup/${backup}/
 
 # nginx备份
 cp -r -f -p /etc/nginx/conf.d/lduoj.conf ${web_home}/lduoj_backup/${backup}/lduoj.nginx.conf
+
 
 echo -e "\nYou have successfully backuped LDU Online Judge!"
 echo -e "Backup location: ${web_home}/lduoj_backup/${backup}/\n"
--- install/ubuntu16.04/install.sh
+++ install/ubuntu16.04/install.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 set -x
-
 web_home=/home    #项目存放位置
-apt -y update && apt -y upgrade
 
-# 下载项目源码
-apt -y install git
-cd ${web_home} && git clone https://github.com/iamwinter/LDUOnlineJudge.git
-apt -y remove git
-cd ${web_home}/LDUOnlineJudge && cp .env.example .env && chmod -R 777 storage bootstrap/cache
+if [ ! -d ${web_home}/LDUOnlineJudge ];then
+  echo No such project: ${web_home}/LDUOnlineJudge
+  exit -1;
+fi;
+cd ${web_home}/LDUOnlineJudge
+
+# 文件权限
+cp -rf .env.example .env && chmod -R 777 storage bootstrap/cache
 
 # php
+apt -y update && apt -y upgrade
 apt -y install software-properties-common python-software-properties
 echo -e "\n" | apt-add-repository ppa:ondrej/php
 apt -y update
@@ -20,13 +22,11 @@ service php7.2-fpm start
 
 # composer
 apt -y install composer zip unzip
-cd ${web_home}/LDUOnlineJudge && composer install --ignore-platform-reqs
+composer install --ignore-platform-reqs
 
 # laravel artisan; 必须在composer之后
 php artisan storage:link
 php artisan key:generate
-php artisan config:clear
-php artisan route:clear
 php artisan config:cache
 php artisan route:cache
 
@@ -49,5 +49,3 @@ mysql -u${USER} -p${PASSWORD} -Dlduoj < ${web_home}/LDUOnlineJudge/install/mysql
 
 
 echo -e "You have successfully installed LDU Online Judge!"
-echo -e "Installation location: ${web_home}/LDUOnlineJudge"
-echo -e "Enjoy it!"
