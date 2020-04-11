bb5a54fd7dcf060a4ee02435ea60d1aa8d5f211e
iamwinter <15866997163@163.com>
2020-04-11 23:26:52 +0800

    LDU Online Judge

--- resources/views/client/problem.blade.php
+++ resources/views/client/problem.blade.php
@@ -273,7 +273,7 @@
             messageStyle: "none", //不显示信息
             jax: ["input/TeX", "output/HTML-CSS"],
             tex2jax: {
-                inlineMath: [["$", "$"], ["\\(", "\\)"]], //行内公式选择符
+                inlineMath: [["\\$", "\\$"], ["\\(", "\\)"]], //行内公式选择符
                 displayMath: [["$$", "$$"], ["\\[", "\\]"]], //段内公式选择符
                 skipTags: ["script", "noscript", "style", "textarea", "pre", "code", "a"] //避开某些标签
             },
