01707f6fde78752093344f0798d085cd4159db96
赵京龙 <winterant@163.com>
2020-05-18 18:03:04 +0800

    LDU Online Judge

--- config/oj/main.php
+++ config/oj/main.php
@@ -1,14 +1,15 @@
 <?php
 
+// 该文件为系统默认配置，后台设置会将新配置保存在数据库中。
 return [
-	"siteName"	=> "Ludong University Online Judge",
-	"beian"	=> "",
-	"web_page_display_wide"	=> false,
-	"allow_register"	=> true,
-	"show_home_notice_marquee"	=> true,
-	"guest_see_problem"	=> true,
-	"rank_show_school"	=> false,
-	"rank_show_nick"	=> true,
-	"penalty_acm"	=> 1200,
-	"submit_interval"	=> 20,
+	"siteName"	                => "Ludong University Online Judge",  //网站名称
+	"beian"	                    => "",      //备案信息
+	"web_page_display_wide" 	=> false,   //宽屏模式
+	"allow_register"	        => true,    //允许访客注册账号
+	"show_home_notice_marquee"	=> true,    //首页顶部滚动显示一条公告
+	"guest_see_problem"	        => true,    //允许访客浏览题目内容
+	"rank_show_school"	        => false,   //榜单显示学校
+	"rank_show_nick"	        => true,    //榜单显示昵称（姓名）
+	"penalty_acm"	            => 1200,    //竞赛acm模式错误一次的罚时，1200秒=20分钟
+	"submit_interval"	        => 20,      //同一用户两次提交最小间隔，20秒
 ];
