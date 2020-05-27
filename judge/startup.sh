902280f913930314b627aa40c21d3d5a35fe4930
赵京龙 <winterant@163.com>
2020-05-27 08:32:02 +0800

    更新dockerfile

--- judge/startup.sh
+++ judge/startup.sh
@@ -6,7 +6,7 @@ bash ./stop.sh
 source ../.env
 
 if [ ! -d ./program ]; then
-    mkdir ./program
+  mkdir ./program
 fi
 
 g++ ./cpp/polling.cpp -o ./program/polling -std=c++11 -lmysqlclient
@@ -14,14 +14,14 @@ g++ ./cpp/judge.cpp  -o  ./program/judge -std=c++11 -lmysqlclient
 
 cd ./program
 if [ "$1" == "debug" ];then
-    ./polling ${DB_HOST} ${DB_PORT} ${DB_USERNAME} ${DB_PASSWORD} ${DB_DATABASE} ${JG_MAX_RUNNING} ${JG_DATA_DIR} ${JG_NAME}
+  ./polling ${DB_HOST} ${DB_PORT} ${DB_USERNAME} ${DB_PASSWORD} ${DB_DATABASE} ${JG_MAX_RUNNING} ${JG_DATA_DIR} ${JG_NAME}
 else
-    ./polling ${DB_HOST} ${DB_PORT} ${DB_USERNAME} ${DB_PASSWORD} ${DB_DATABASE} ${JG_MAX_RUNNING} ${JG_DATA_DIR} ${JG_NAME} > /dev/null &
+  ./polling ${DB_HOST} ${DB_PORT} ${DB_USERNAME} ${DB_PASSWORD} ${DB_DATABASE} ${JG_MAX_RUNNING} ${JG_DATA_DIR} ${JG_NAME} > /dev/null &
     sleep 1;
     polling_name=`ps -e | grep polling | awk '{print $4}'`
     if [ "${polling_name}" == "polling" ];then
-        echo "[Judge is OK] Server has started to judge!"
+      echo "[Judge is OK] Server has started to judge!"
     else
-        echo "[Judge starting Failed] Please check config in LDUOnlineJudge/.env"
+      echo "[Judge starting Failed] Please check config in LDUOnlineJudge/.env"
     fi
 fi
