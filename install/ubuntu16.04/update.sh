92047e873717b7b03ddf12e0b667b97af927b0ed
赵京龙 <winterant@163.com>
2020-06-18 15:47:18 +0800

    整改升级方式

--- install/ubuntu16.04/update.sh
+++ install/ubuntu16.04/update.sh
@@ -10,17 +10,19 @@ if [ ! -d ${web_home}/lduoj_upgrade ];then
 fi;
 
 # transfer files
-rm -rf ${web_home}/lduoj_upgrade/storage
-mv -f ${web_home}/LDUOnlineJudge/storage ${web_home}/lduoj_upgrade/
-mv -f ${web_home}/LDUOnlineJudge/vendor  ${web_home}/lduoj_upgrade/
-mv -f ${web_home}/LDUOnlineJudge/.env    ${web_home}/lduoj_upgrade/
-mv -f ${web_home}/LDUOnlineJudge/public/favicon.ico  ${web_home}/lduoj_upgrade/public/
-rm -rf ${web_home}/LDUOnlineJudge/*
-mv ${web_home}/lduoj_upgrade/*  ${web_home}/LDUOnlineJudge/
-rm -rf ${web_home}/lduoj_upgrade
+mv -f ${web_home}/lduoj_upgrade/app        ${web_home}/LDUOnlineJudge/
+mv -f ${web_home}/lduoj_upgrade/config     ${web_home}/LDUOnlineJudge/
+mv -f ${web_home}/lduoj_upgrade/install    ${web_home}/LDUOnlineJudge/
+mv -f ${web_home}/lduoj_upgrade/judge      ${web_home}/LDUOnlineJudge/
+mv -f ${web_home}/lduoj_upgrade/public     ${web_home}/LDUOnlineJudge/
+mv -f ${web_home}/lduoj_upgrade/resources  ${web_home}/LDUOnlineJudge/
+mv -f ${web_home}/lduoj_upgrade/routes     ${web_home}/LDUOnlineJudge/
+mv -f ${web_home}/lduoj_upgrade/.env.example     ${web_home}/LDUOnlineJudge/
+mv -f ${web_home}/lduoj_upgrade/composer.json    ${web_home}/LDUOnlineJudge/
+mv -f ${web_home}/lduoj_upgrade/composer.lock    ${web_home}/LDUOnlineJudge/
+m -rf ${web_home}/lduoj_upgrade
 
 cd ${web_home}/LDUOnlineJudge || exit 2;
-chmod -R 777 storage bootstrap/cache
 
 # update packages
 composer install --ignore-platform-reqs
