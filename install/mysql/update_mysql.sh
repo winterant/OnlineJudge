bb8fbc3b8f51f4b7126a7b3e9d19c3341c12316c
赵京龙 <winterant@163.com>
2020-07-09 08:41:38 +0800

    修复mysql更新bug

--- install/mysql/update_mysql.sh
+++ install/mysql/update_mysql.sh
@@ -9,5 +9,5 @@ PASSWORD=`cat /etc/mysql/debian.cnf |grep password|head -1|awk '{print $3}'`
 mysql -u${USER} -p${PASSWORD} -e"DROP DATABASE IF EXISTS lduoj_upgrade;"
 mysql -u${USER} -p${PASSWORD} -e"CREATE DATABASE IF NOT EXISTS lduoj_upgrade;"
 mysql -u${USER} -p${PASSWORD} -Dlduoj_upgrade < ${web_home}/LDUOnlineJudge/install/mysql/lduoj.sql
-php ${web_home}/LDUOnlineJudge/install/mysql/structure_sync.php ${USER} ${PASSWORD} | mysql -u${USER} -p${PASSWORD} -Dlduoj
+php ${web_home}/LDUOnlineJudge/install/mysql/structure_sync.php ${USER} ${PASSWORD} | mysql -u${USER} -p${PASSWORD} -Dlduoj -v
 mysql -u${USER} -p${PASSWORD} -e"DROP DATABASE IF EXISTS lduoj_upgrade;"
