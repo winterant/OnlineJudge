f9c6d5510c0ab4d5673721413c7d130b46e7ed55
赵京龙 <winterant@163.com>
2020-08-09 12:03:03 +0800

    更换语言切换的方式

--- app/Http/Middleware/SetTimeZone.php
+++ app/Http/Middleware/SetTimeZone.php
@@ -17,6 +17,7 @@ class SetTimeZone
     {
         //date()函数的时区默认UTC，用这个全局中间件来改为上海时间
         date_default_timezone_set('Asia/Shanghai');
+        App::setLocale(get_setting('APP_LOCALE','en')); //设置语言
         return $next($request);
     }
 }
