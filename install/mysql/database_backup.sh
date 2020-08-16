83c925a62d49a52f80acf591e9c9cf422b2a64fc
赵京龙 <winterant@163.com>
2020-08-16 22:35:56 +0800

    update

--- install/mysql/database_backup.sh
+++ install/mysql/database_backup.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-if [ -n "$1" ]; then
-    back_dir=.
-else
-    back_dir=$1
-fi
-
 USER=`cat /etc/mysql/debian.cnf |grep user|head -1|awk '{print $3}'`
 PASSWORD=`cat /etc/mysql/debian.cnf |grep password|head -1|awk '{print $3}'`
-mysqldump -u${USER} -p${PASSWORD} -B lduoj > ${back_dir}/lduoj_backup.sql
+mysqldump -u${USER} -p${PASSWORD} -B lduoj > $(dirname $0)/lduoj.sql
 
-echo "Generated back-up: ${back_dir}/lduoj_backup.sql"
+echo "Generated back-up: $(dirname $0)/lduoj.sql"
