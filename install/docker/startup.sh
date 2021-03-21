be288d10658ad6424a61ac7b454a995fb31cacd4
赵京龙 <winterant@163.com>
2021-03-21 14:50:48 +0800

    修改：docker安装

--- install/docker/startup.sh
+++ install/docker/startup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if [ ! -d /volume/LDUOnlineJudge ]; then
+    mv /home/LDUOnlineJudge /volume/
+else
+    rm -rf /home/LDUOnlineJudge
+fi
+ln -s /volume/LDUOnlineJudge /home/LDUOnlineJudge
+
+if [ ! -d /volume/mysql ]; then
+    mv /var/lib/mysql /volume/
+else
+    rm -rf /var/lib/mysql
+fi
+ln -s /volume/mysql /var/lib/mysql
+chown -R mysql:mysql /volume/mysql
 rm -rf /var/run/mysqld/mysqld.sock.lock
 
 service nginx start
