6646218f9574f8dc210a07dd09afe262193d1e2b
赵京龙 <winterant@163.com>
2021-08-07 20:56:09 +0800

    调整 升级时不再删除旧文件，防止因升级失败而无法找回旧文件

--- install/ubuntu16.04/update.sh
+++ install/ubuntu16.04/update.sh
@@ -8,32 +8,13 @@ if [[ "${APP_HOME}" == "" ]]; then
     read -p "Please input old project location (such as /home/LDUOnlineJudge):" APP_HOME
 fi
 
-# transfer files
-rm -rf ${APP_HOME}/.git
-rm -rf ${APP_HOME}/app
-rm -rf ${APP_HOME}/config
-rm -rf ${APP_HOME}/install/{docker,mysql,ubuntu16.04}
-rm -rf ${APP_HOME}/judge/{cpp,java.policy,startup.sh,stop.sh}
-rm -rf ${APP_HOME}/public/{css,images,js,static}
-rm -rf ${APP_HOME}/resources
-rm -rf ${APP_HOME}/routes
-
-mv -f ${upgrade}/.git       ${APP_HOME}/
-mv -f ${upgrade}/app        ${APP_HOME}/
-mv -f ${upgrade}/config     ${APP_HOME}/
-mv -f ${upgrade}/install/{docker,mysql,ubuntu16.04}    ${APP_HOME}/install/
-mv -f ${upgrade}/judge/{cpp,java.policy,startup.sh,stop.sh}      ${APP_HOME}/judge/
-mv -f ${upgrade}/public/{css,images,js,static}     ${APP_HOME}/public/
-mv -f ${upgrade}/resources  ${APP_HOME}/
-mv -f ${upgrade}/routes     ${APP_HOME}/
-mv -f ${upgrade}/composer.json ${APP_HOME}/
-mv -f ${upgrade}/composer.lock ${APP_HOME}/
-mv -f ${upgrade}/.env.example  ${APP_HOME}/
-mv -f ${upgrade}/readme.md     ${APP_HOME}/
-
+# update files
+cp -rf "${APP_HOME}"/install/nginx/lduoj.conf "${upgrade}"/install/nginx/
+cp -rf "${APP_HOME}"/public/favicon.ico       "${upgrade}"/public/
+cp -rf "${upgrade}"/*                         "${APP_HOME}"/
 
 # update laravel packages
-cd ${APP_HOME} || exit 2
+cd "${APP_HOME}" || exit 2
 chmod -R 755 storage bootstrap/cache
 chown www-data:www-data -R storage bootstrap/cache
 composer install --ignore-platform-reqs
@@ -42,7 +23,7 @@ php artisan key:generate
 php artisan optimize
 
 # update mysql table schema
-bash ${APP_HOME}/install/mysql/update_mysql.sh
+bash "${APP_HOME}"/install/mysql/update_mysql.sh
 
 # docker startup
 if [ -f /.dockerenv ]; then
@@ -55,10 +36,10 @@ fi
 
 # If not in docker, start to judge
 if [ ! -f /.dockerenv ]; then
-    bash ${APP_HOME}/judge/startup.sh
+    bash "${APP_HOME}"/judge/startup.sh
 fi
 
 # delete upgrade
-rm -rf ${upgrade} &
+rm -rf "${upgrade}" &
 
 echo "You have successfully updated LDU Online Judge! Enjoy it!\n"
