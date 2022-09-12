c0197fc508272ab9164a34b67612e578f3664c2b
Jinglong <winterant@163.com>
2022-09-12 13:49:50 +0800

    optimize ckeditor upload controller

--- app/Http/Controllers/Api/UploadController.php
+++ app/Http/Controllers/Api/UploadController.php
@@ -9,16 +9,17 @@ use Illuminate\Support\Facades\Storage;
 class UploadController extends Controller
 {
     // CKeditor 上传图片
-    public function ck_upload_image(Request $request){
-        $image=$request->file('upload'); //ckeditor传过来的文件名upload
-        $allowed_ext = ["jfif", "pjpeg", "jpeg", "pjp", "jpg", "png", ".gif", "bmp", "dib", "webp", "tiff", "tif"];
-        if(in_array($image->getClientOriginalExtension(), $allowed_ext)){
-            $fdir='public/ckeditor/images';  //保存文件夹
-            $fname=uniqid(date('YmdHis_')).'.'.$image->getClientOriginalExtension();//保存名
-            $image->move(storage_path('app/'.$fdir),$fname);
-            return json_encode(['uploaded'=>true,'url'=> Storage::url($fdir.'/'.$fname)]);
+    public function ck_upload_image(Request $request)
+    {
+        $image = $request->file('upload'); //ckeditor传过来的文件 upload
+        $allowed_ext = ["jfif", "pjpeg", "jpeg", "pjp", "jpg", "png", "gif", "bmp", "dib", "webp", "tiff", "tif"];
+        if (in_array($image->getClientOriginalExtension(), $allowed_ext)) {
+            $fdir = 'public/ckeditor/images/' . date('Ym');  //保存文件夹
+            $fname = uniqid(date('YmdHis_')) . '.' . $image->getClientOriginalExtension(); //保存名
+            $image->move(storage_path('app/' . $fdir), $fname);
+            return json_encode(['uploaded' => true, 'url' => Storage::url($fdir . '/' . $fname)]);
         }
         //图片格式有误，不允许上传，杜绝上传有害脚本的可能
-        return json_encode(['uploaded'=>false,'url'=> null]);
+        return json_encode(['uploaded' => false, 'url' => null]);
     }
 }
