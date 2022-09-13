46ad0a9656f4a09987c29287ad6c2befa0fe4941
Jinglong <winterant@163.com>
2022-09-13 14:37:25 +0800

    添加拓展php7.2-zip

--- app/Jobs/QueryJudge0Result.php
+++ app/Jobs/QueryJudge0Result.php
@@ -2,7 +2,6 @@
 
 namespace App\Jobs;
 
-use App\Http\Controllers\Api\SolutionController;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
