094041866434796e83790b4c0bfd27d21cdb4161
Jinglong <winterant@163.com>
2022-09-16 15:28:14 +0800

    test

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -9,42 +9,49 @@ use Illuminate\Support\Facades\Storage;
 
 class HomeController extends Controller
 {
+    public function test(Request $request)
+    {
+        return intdiv(10, 3);
+    }
+
     //home page
-    public function index(){
-        $notices=DB::table('notices')
-            ->leftJoin('users','users.id','=','user_id')
-            ->select(['notices.id','title','state','notices.created_at','username'])
-            ->where('state','!=',0)
+    public function index()
+    {
+        $notices = DB::table('notices')
+            ->leftJoin('users', 'users.id', '=', 'user_id')
+            ->select(['notices.id', 'title', 'state', 'notices.created_at', 'username'])
+            ->where('state', '!=', 0)
             ->orderByDesc('state')
             ->orderByDesc('id')->paginate(6);
 
-        $day=(date("w")+6)%7; //昨天是周几，周日=0
-        $this_week=DB::table('solutions')
-            ->join('users','users.id','=','solutions.user_id')
-            ->select(['user_id','username','school','class','nick',DB::raw('count(distinct problem_id) as solved'),])
-            ->where('submit_time','>',date('Y-m-d 00:00:00',time()-3600*24*$day))
-            ->where('result',4)
+        $day = (date("w") + 6) % 7; //昨天是周几，周日=0
+        $this_week = DB::table('solutions')
+            ->join('users', 'users.id', '=', 'solutions.user_id')
+            ->select(['user_id', 'username', 'school', 'class', 'nick', DB::raw('count(distinct problem_id) as solved'),])
+            ->where('submit_time', '>', date('Y-m-d 00:00:00', time() - 3600 * 24 * $day))
+            ->where('result', 4)
             ->whereRaw("(select count(*) from privileges P where solutions.user_id=P.user_id and authority='admin')=0")
             ->groupBy(['user_id'])
             ->orderByDesc('solved')
             ->limit(10)->get();
-        $last_week=DB::table('solutions')
-            ->join('users','users.id','=','solutions.user_id')
-            ->select(['user_id','username','school','class','nick',DB::raw('count(distinct problem_id) as solved')])
-            ->where('submit_time','>',date('Y-m-d 00:00:00',time()-3600*24*($day+7)))
-            ->where('submit_time','<',date('Y-m-d 00:00:00',time()-3600*24*$day))
-            ->where('result',4)
+        $last_week = DB::table('solutions')
+            ->join('users', 'users.id', '=', 'solutions.user_id')
+            ->select(['user_id', 'username', 'school', 'class', 'nick', DB::raw('count(distinct problem_id) as solved')])
+            ->where('submit_time', '>', date('Y-m-d 00:00:00', time() - 3600 * 24 * ($day + 7)))
+            ->where('submit_time', '<', date('Y-m-d 00:00:00', time() - 3600 * 24 * $day))
+            ->where('result', 4)
             ->whereRaw("(select count(*) from privileges P where solutions.user_id=P.user_id and authority='admin')=0")
             ->groupBy(['user_id'])
             ->orderByDesc('solved')
             ->limit(10)->get();
-        return view('client.home',compact('notices','this_week','last_week'));
+        return view('client.home', compact('notices', 'this_week', 'last_week'));
     }
 
-    public function get_notice(Request $request){
-        $notice=DB::table('notices')->select(['title','content','created_at'])->find($request->input('id'));
-        if ($notice && $notice->content==null)
-            $notice->content='';
+    public function get_notice(Request $request)
+    {
+        $notice = DB::table('notices')->select(['title', 'content', 'created_at'])->find($request->input('id'));
+        if ($notice && $notice->content == null)
+            $notice->content = '';
         return json_encode($notice);
     }
 }
