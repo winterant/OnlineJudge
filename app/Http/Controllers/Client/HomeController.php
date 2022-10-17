006820afe92ecfa0bc515b19e4abb9789f528c04
Jinglong <winterant@163.com>
2022-10-17 11:08:50 +0800

    修复 首页top更新时间不对

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -24,19 +24,23 @@ class HomeController extends Controller
             ->orderByDesc('state')
             ->orderByDesc('id')->paginate(6);
 
+        // 获取周一时间. 如果今天是周一则为今天，否则为最近一次周一
+        $monday_time = (date('w') == 1 ? strtotime('today') : strtotime('last monday'));
+        $last_monday_time = $monday_time - 3600 * 24 * 7;
+        $next_monday_time = $monday_time + 3600 * 24 * 7;
         if (Redis::exists('home:cache:this_week_top')) {
             $this_week = json_decode(Redis::get('home:cache:this_week_top'));
         } else {
             $this_week = DB::table('solutions')
                 ->join('users', 'users.id', '=', 'solutions.user_id')
                 ->select(['user_id', 'username', 'school', 'class', 'nick', DB::raw('count(distinct problem_id) as solved'),])
-                ->where('submit_time', '>', date('Y-m-d H:i:s', strtotime('last monday')))
+                ->where('submit_time', '>', date('Y-m-d H:i:s', $monday_time))
                 ->where('result', 4)
                 ->groupBy(['user_id'])
                 ->orderByDesc('solved')
                 ->limit(10)->get();
-            // 缓存有效期至凌晨
-            Redis::setex('home:cache:this_week_top', strtotime('tomorrow') - time(), json_encode($this_week));
+            // 缓存有效期1小时
+            Redis::setex('home:cache:this_week_top', 3600, json_encode($this_week));
         }
 
         if (Redis::exists('home:cache:last_week_top')) {
@@ -45,15 +49,15 @@ class HomeController extends Controller
             $last_week = DB::table('solutions')
                 ->join('users', 'users.id', '=', 'solutions.user_id')
                 ->select(['user_id', 'username', 'school', 'class', 'nick', DB::raw('count(distinct problem_id) as solved')])
-                ->where('submit_time', '>', date('Y-m-d H:i:s', strtotime('last monday') - 3600 * 24 * 7))
-                ->where('submit_time', '<', date('Y-m-d H:i:s', strtotime('last monday')))
+                ->where('submit_time', '>', date('Y-m-d H:i:s', $last_monday_time))
+                ->where('submit_time', '<', date('Y-m-d H:i:s', $monday_time))
                 ->where('result', 4)
                 // ->whereRaw("(select count(*) from privileges P where solutions.user_id=P.user_id and authority='admin')=0")
                 ->groupBy(['user_id'])
                 ->orderByDesc('solved')
                 ->limit(10)->get();
             // 缓存有效期至下周一
-            Redis::setex('home:cache:last_week_top', strtotime('next monday') - time(), json_encode($last_week));
+            Redis::setex('home:cache:last_week_top', $next_monday_time - time(), json_encode($last_week));
         }
         return view('client.home', compact('notices', 'this_week', 'last_week'));
     }
