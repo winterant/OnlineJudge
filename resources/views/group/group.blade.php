3aa674ce4f64f80ac156e3671471abbb24847863
Jinglong <winterant@163.com>
2022-12-22 16:51:30 +0800

    修正 群组显示列标题有误

--- resources/views/group/group.blade.php
+++ resources/views/group/group.blade.php
@@ -49,7 +49,7 @@
                     <th>{{ __('main.Order') }}</th>
                   @endif
                   <th nowrap>{{ trans('main.Title') }}</th>
-                  <th nowrap>{{ __('main.Access') }}</th>
+                  <th nowrap>{{ __('main.ranking_rule') }}</th>
                   {{-- <th nowrap>{{ __('main.ranking_rule') }}</th> --}}
                   <th nowrap>{{ __('main.Time') }}</th>
                   <th nowrap>{{ __('main.Contestants') }}</th>
