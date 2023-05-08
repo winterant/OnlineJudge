8eb1c0a045a426ae6a9d4d8b9c540455cd6424ef
Jinglong <winterant@163.com>
2023-05-08 20:23:32 +0800

    修复 错误数据无法查看

--- resources/views/solution/solution_wrong_data.blade.php
+++ resources/views/solution/solution_wrong_data.blade.php
@@ -2,12 +2,17 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <x-head />
-    <title>Wrong data</title>
+  <style>
+    pre {
+      margin: 0px;
+      font-size: 1rem;
+    }
+  </style>
+  <title>Wrong data</title>
 </head>
 
 <body>
-    <pre>{{ $text }}</pre>
+  <pre>{{ $text }}</pre>
 </body>
 
 </html>
